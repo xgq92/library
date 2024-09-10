ac3ec82c4435f853327c95070f0bc8fe0ee6f279
xuguoqiang <xuguoqiang@gw.com.cn>
2024-09-10 09:42:37 +0800

    modify getbook

--- service/book/getBooks.go
+++ service/book/getBooks.go
@@ -18,10 +18,10 @@ func GetBooks(ctx context.Context, req *library.GetBooksReq) (*library.GetBooksR
 	rets := []internal.BookInfo{}
 	var count int32
 	// data.GetDb().Where(where, req.GetCode(), req.GetName()).Find(&rets).Count(&count)
-	data.GetDb().Where("book_counts <> 0").Where(where).Find(&rets).Count(&count)
+	data.GetDb().Where(where).Where("book_counts <> 0").Find(&rets).Count(&count)
 	if count != 0 {
 		ret := data.GetDb().Limit(req.GetLimit()).Offset(req.GetOffset()).
-			Where("book_counts <> 0").Where(where).Find(&rets)
+			Where(where).Where("book_counts <> 0").Find(&rets)
 		if ret.Error != nil {
 			logger.Error(`DeleteBook db.Delete error: %v`, ret.Error)
 			return nil, ret.Error
